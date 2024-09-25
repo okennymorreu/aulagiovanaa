3e0737783fa39877b9b7e571f44587718436d705
okennymorreu <142039412+okennymorreu@users.noreply.github.com>
2024-09-25 08:25:36 -0300

    nn

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ console.log (soma(3,2));
 function areaQuadrado (lado) {
  return lado**2
 }
+
 console.log (areaQuadrado (6));
 let media= function( ) {
 return  ;
- console.log ( );
+ console.log ( )
 }
