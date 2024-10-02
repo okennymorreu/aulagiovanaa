0d628a5711a928657dfd35e4779822c68d52a721
okennymorreu <142039412+okennymorreu@users.noreply.github.com>
2024-10-02 08:17:29 -0300

    d

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log(estados[i]);
 i=i+1;
 }
 
-for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < estados.length; i++) {
     console.log(i);
     console.log (estados[i]);
     
